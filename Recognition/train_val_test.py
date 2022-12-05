451e41f8e602190d9441db40264511e2b842e84c
Artsiom Stalbanau <110119436+ArtsiomSt@users.noreply.github.com>
2022-12-05 09:38:10 +0300

    Update train_val_test.py

--- Recognition/train_val_test.py
+++ Recognition/train_val_test.py
@@ -4,9 +4,8 @@ import shutil
 import math
 import random
 
-
-base_dir = 'path to dir with raw data'
-to_dir = 'Path to train dir'
+base_dir = 'C:\\Users\\arteo\\Desktop\\Fnt\\'
+to_dir = 'C:\\Users\\arteo\\Desktop\\NoNumTrainValidTest'
 
 # symbol = os.path.join(base_dir, 'Sample001\\48-0.png')
 test_p = 0.15
@@ -14,41 +13,39 @@ valid_p = 0.15
 train_p = 0.7
 out = 32
 
-
-# def get_file_names(dir, proc):
-#     files = os.listdir(dir)
-#     valid_files = files[math.floor():]
-#     return
-#
-# get_file_names(base_dir, 1)
-
+not_allowed_dirs = [str(x) for x in range(11)]
+not_allowed_dirs.append('45')
 
 
 def create_train():
     os.mkdir(os.path.join(to_dir, 'train'))
     to_dir_new = os.path.join(to_dir, 'train')
     for dir in os.listdir(base_dir):
+        if dir.lstrip('Sample').lstrip('0') in not_allowed_dirs:
+            continue
         cur_dir = os.path.join(base_dir, dir)
         if dir not in os.listdir(to_dir):
             os.mkdir(os.path.join(to_dir_new, dir))
         to_dir_new_cpy = os.path.join(to_dir_new, dir)
         list_of_files = os.listdir(os.path.join(base_dir, dir))
-        for file in list_of_files[:math.floor(len(list_of_files)*train_p)]:
+        for file in list_of_files[:math.floor(len(list_of_files) * train_p)]:
             shutil.copy2(os.path.join(cur_dir, file), to_dir_new_cpy)
-
 
 
 def create_valid():
     os.mkdir(os.path.join(to_dir, 'valid'))
     to_dir_new = os.path.join(to_dir, 'valid')
     for dir in os.listdir(base_dir):
+        if dir.lstrip('Sample').lstrip('0') in not_allowed_dirs:
+            continue
         cur_dir = os.path.join(base_dir, dir)
         if dir not in os.listdir(to_dir):
             os.mkdir(os.path.join(to_dir_new, dir))
         to_dir_new_cpy = os.path.join(to_dir_new, dir)
         list_of_files = os.listdir(os.path.join(base_dir, dir))
-        # print(math.floor(len(list_of_files)*train_p), math.floor(len(list_of_files)*train_p)+math.floor(len(list_of_files)*valid_p))
-        for file in list_of_files[math.floor(len(list_of_files)*train_p):math.floor(len(list_of_files)*train_p)+math.floor(len(list_of_files)*valid_p)]:
+        for file in list_of_files[
+                    math.floor(len(list_of_files) * train_p):math.floor(len(list_of_files) * train_p) + math.floor(
+                            len(list_of_files) * valid_p)]:
             shutil.copy2(os.path.join(cur_dir, file), to_dir_new_cpy)
 
 
@@ -56,12 +53,14 @@ def create_test():
     os.mkdir(os.path.join(to_dir, 'test'))
     to_dir_new = os.path.join(to_dir, 'test')
     for dir in os.listdir(base_dir):
+        if dir.lstrip('Sample').lstrip('0') in not_allowed_dirs:
+            continue
         cur_dir = os.path.join(base_dir, dir)
         if dir not in os.listdir(to_dir):
             os.mkdir(os.path.join(to_dir_new, dir))
         to_dir_new_cpy = os.path.join(to_dir_new, dir)
         list_of_files = os.listdir(os.path.join(base_dir, dir))
-        for file in list_of_files[math.floor(len(list_of_files)*train_p)+math.floor(len(list_of_files)*valid_p):]:
+        for file in list_of_files[math.floor(len(list_of_files) * train_p) + math.floor(len(list_of_files) * valid_p):]:
             shutil.copy2(os.path.join(cur_dir, file), to_dir_new_cpy)
 
 
@@ -102,9 +101,34 @@ def count_file():
     return answer
 
 
+def delete_688_691():
+    not_allowed_nums = ['688', '689', '690', '691', '629', '630', '631', '628', '612', '613', '292', '293', '294',
+                        '295', '384', '385', '386', '387', '400', '401', '402', '403', '528', '529', '530', '531',
+                        '104', '106', '105', '107', '160', '161', '162', '163', '164', '260', '261', '262', '263',
+                        '460', '461', '462', '463', '254', '255', '506', '507', '484', '485', '486', '487']
+    for outer_dir in os.listdir(to_dir):
+        outer_dir_for_files = os.path.join(to_dir, outer_dir)
+        for inner_dir in os.listdir(os.path.join(to_dir, outer_dir)):
+            inner_dir_for_files = os.path.join(outer_dir_for_files, inner_dir)
+            for file in os.listdir(inner_dir_for_files):
+                cur_file = os.path.join(inner_dir_for_files, file)
+                if check_mas_el_in_str(file, not_allowed_nums):
+                    os.remove(cur_file)
+
+
+def check_mas_el_in_str(str_for_test: str, not_allowed: list) -> bool:
+    for item in not_allowed:
+        if item in str_for_test:
+            return True
+    return False
+
+
+#
 # create_train()
 # create_valid()
 # create_test()
-# print(count_file())
-
-refactor_to()
+print(count_file())
+#
+# refactor_to()
+#
+# delete_688_691()
